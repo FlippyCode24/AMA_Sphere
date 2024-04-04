33a382ea4e2baf7f3067e61597ef60770805c8b4
VInce Joshua <147134893+Seijuro-V@users.noreply.github.com>
2024-04-04 14:32:47 +0800

    Update utils.py

--- admin/user/utils.py
+++ admin/user/utils.py
@@ -5,5 +5,5 @@ import random
 def Generated_lotto_draw(): #function name
     numbers =  sorted(random.sample(range(1, 51), 6))
     #list that generate 6 unique numbers between 1 to 51
-    return ', '.join(str(num) for num in numbers) 
+    return ', '.join(str(numbers) for num in numbers) 
     #return a string with containing the lotto numbers with ',' seperator
